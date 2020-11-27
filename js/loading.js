3c635d3c108955aadb4d017d8e7c376446b9d473
Marcos <marcosazevedo2112@gmail.com>
2020-11-27 00:58:40 -0300

    bibliografia quase pronta
    Co-authored-by: Yuri Estevão <yesales07@gmail.com>
    Co-authored-by: Gustavo Arc <arcgustavo10@gmail.com>
    Co-authored-by: Luiz Fernando Almeida <cartolaluiz@yahoo.com.br>

---
+++ js/loading.js
@@ -0,0 +1,6 @@
+var i = setInterval(function () {
+    clearInterval(i);
+
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("conteudo").style.display = "inline";
+}, 2000);
